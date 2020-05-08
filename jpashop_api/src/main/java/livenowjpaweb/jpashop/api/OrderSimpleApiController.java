1dbfcba0ecbeaee612688e33bfe0f9105ac40cc2
Livenow14 <xowjd41@naver.com>
2020-05-08 18:26:53 +0900

    API 개발 고급_지연 로딩과 조회 성능 최적화_ 간단한 주문 조회: V2: 엔티티를 DTO로 변환

--- jpashop_api/src/main/java/livenowjpaweb/jpashop/api/OrderSimpleApiController.java
+++ jpashop_api/src/main/java/livenowjpaweb/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package livenowjpaweb.jpashop.api;
 
+import livenowjpaweb.jpashop.domain.Address;
 import livenowjpaweb.jpashop.domain.Order;
+import livenowjpaweb.jpashop.domain.OrderStatus;
 import livenowjpaweb.jpashop.repository.OrderRepository;
 import livenowjpaweb.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * x To One 관계(ManyToOne, OneToOne)
@@ -27,11 +32,50 @@ public class OrderSimpleApiController {
      * @return
      */
 
-    @GetMapping("/api/v1/simple-orders")                                                            //무한루프에 빠지는 예제  이거는 따로하지않는다.
+/*    @GetMapping("/api/v1/simple-orders")                                                            //무한루프에 빠지는 예제  이거는 따로하지않는다.
     public List<Order> orderV1(){                                                                  // 이렇게 하면 절대 안되기때문에 진행하지 않는다.
         List<Order> all = orderRepository.findAllByString(new OrderSearch());                       //DTO로 변환해서 사용하는게 좋다.
         return all;
 
+    }*/
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     *  - 단점: 지연로딩으로 쿼리 N번 호출
+    */
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        //ORDER(N) 2개
+        // N + 1 -> 1 + 회원 N + 배송 N 총 5번 호출됨
+        // 지연로딩을 무조건 써야한다. 영속성 컨텍스트를 위해 같은 멤버가 주문했다면 4번으로 변함.
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId=order.getId();
+            name = order.getMember().getName();     //LAZY 초기화됨
+            orderDate = order.getOrderDate();
+            orderStatus= order.getStatus();
+            address = order.getDelivery().getAddress();     //LAZY 초기화됨
+
+
+        }
     }
 
 
